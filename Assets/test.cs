dfd6466ca3dde1d9589f4d990cf9620f29df3ff4
devastadus <scottprice7@gmail.com>
2016-07-10 21:35:21 -0700

    initial update

---
+++ Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position += new Vector3(0,-1,0);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
